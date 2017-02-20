fcd015621d41a8bab0c1707d4d7c700c2200f4f5
Cara Delorey <deloreyc@tcd.ie>
2017-02-20 13:51:37 +0000

    detail what setup.py requires

---
+++ skeleton/setup.py
@@ -0,0 +1,5 @@
+"""Your setup.py should install a script which reads input from a file (perhaps over
+the network), specified as a command line argument:
+1 solve_led --input http :// claritytrec.ucd.ie/~ alawlor/comp30670/ -
+input_assign3.txt
+"""
